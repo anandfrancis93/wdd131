b8d11ea74e1ce92e52044f1b5f17cd9074b63de6
Anand Francis <anand.francis93@gmail.com>
2026-02-09 22:00:23 -0700

    Refactor responsive gallery to match requirements

--- week4/ponder/responsive.js
+++ week4/ponder/responsive.js
@@ -1,7 +1,26 @@
-const hamButton = document.querySelector('.menu-btn');
-const navigation = document.querySelector('nav');
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if (e.target.tagName === 'IMG') {
+        const src = e.target.getAttribute('src');
+        const newSrc = src.replace('-sm.jpg', '-full.jpg');
+        modalImage.setAttribute('src', newSrc);
+        modalImage.setAttribute('alt', e.target.alt);
+        modal.showModal();
+    }
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
 });
